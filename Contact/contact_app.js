d89a41fb3403950eaead4e5c1b040471f9c24f40
gollobc <cadegollob@gmail.com>
2023-06-12 08:41:58 -0700

    little bit of commenting in js

--- Contact/contact_app.js
+++ Contact/contact_app.js
@@ -1,8 +1,9 @@
+
+// Grabbing images in footer and linking them to their respective sites
 const gitHub = document.getElementById('github')
 gitHub.addEventListener('click', () => {
     window.open('https://www.github.com/gollobc', '_blank','noreferrer', 'noopener')
 })
-
 const linkedIn = document.getElementById('linkedin')
 linkedIn.addEventListener('click', () => {
     window.open('https://www.linkedin.com/in/cadegollob', '_blank', 'noreferrer', 'noopener')
--- Home/home_app.js
+++ Home/home_app.js
@@ -1,14 +1,14 @@
-
+// Grabbing images in footer and linking them to their respective sites
 const gitHub = document.getElementById('github')
 gitHub.addEventListener('click', () => {
     window.open('https://www.github.com/gollobc', '_blank','noreferrer', 'noopener')
 })
-
 const linkedIn = document.getElementById('linkedin')
 linkedIn.addEventListener('click', () => {
     window.open('https://www.linkedin.com/in/cadegollob', '_blank', 'noreferrer', 'noopener')
 })
 
+//Runs when resume button is clicked. Creates and iframe and appends the new element below the button with a button to clear the iframe.
 function openR() {
     let resume = document.createElement('IFRAME')
     resume.setAttribute('src', '../Resumenewest.docx.pdf')
@@ -37,22 +37,22 @@ let previousImgIndex = 0;
 next.addEventListener('click', () => {
     previousImgIndex = currentImgIndex
     currentImgIndex++
-
+    //Starting images over if you click next on last image.
     if (currentImgIndex >= images.length) {
         currentImgIndex = 0;
     }
-
+    //Changing what image is displayed when button is clicked
     images[previousImgIndex].style.display = 'none'
     images[currentImgIndex].style.display = 'block'
 })
 prev.addEventListener('click', () => {
     previousImgIndex = currentImgIndex
     currentImgIndex--
-
+    //Starting images at last image if you click prev on first image.
     if (currentImgIndex < 0) {
         currentImgIndex = images.length - 1;
     }
-
+    //Changing what image is displayed when button is clicked
     images[previousImgIndex].style.display = 'none'
     images[currentImgIndex].style.display = 'block'
 })
