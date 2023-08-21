09c484b6318524ae8fb0286e8ff11f79af980d38
gollobc <cadegollob@gmail.com>
2023-08-21 00:59:37 -0700

    updated

--- Contact/contact_app.js
+++ Contact/contact_app.js
@@ -1,10 +1,21 @@
 
 // Grabbing images in footer and linking them to their respective sites
-const gitHub = document.getElementById('github')
-gitHub.addEventListener('click', () => {
-    window.open('https://www.github.com/gollobc', '_blank','noreferrer', 'noopener')
-})
-const linkedIn = document.getElementById('linkedin')
-linkedIn.addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/cadegollob', '_blank', 'noreferrer', 'noopener')
+// const gitHub = document.getElementById('github')
+// gitHub.addEventListener('click', () => {
+//     window.open('https://www.github.com/gollobc', '_blank','noreferrer', 'noopener')
+// })
+// const linkedIn = document.getElementById('linkedin')
+// linkedIn.addEventListener('click', () => {
+//     window.open('https://www.linkedin.com/in/cadegollob', '_blank', 'noreferrer', 'noopener')
+// })
+
+const yourName = document.getElementById("name")
+const email = document.getElementById('email')
+const comments = document.getElementById('comments')
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', () => {
+    email.value = ''
+    yourName.value = ''
+    comments.value = ''
 })
--- Home/home_app.js
+++ Home/home_app.js
@@ -1,12 +1,12 @@
 // Grabbing images in footer and linking them to their respective sites
-const gitHub = document.getElementById('github')
-gitHub.addEventListener('click', () => {
-    window.open('https://www.github.com/gollobc', '_blank','noreferrer', 'noopener')
-})
-const linkedIn = document.getElementById('linkedin')
-linkedIn.addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/cadegollob', '_blank', 'noreferrer', 'noopener')
-})
+// const gitHub = document.getElementById('github')
+// gitHub.addEventListener('click', () => {
+//     window.open('https://www.github.com/gollobc', '_blank','noreferrer', 'noopener')
+// })
+// const linkedIn = document.getElementById('linkedin')
+// linkedIn.addEventListener('click', () => {
+//     window.open('https://www.linkedin.com/in/cadegollob', '_blank', 'noreferrer', 'noopener')
+// })
 
 //Runs when resume button is clicked. Creates and iframe and appends the new element below the button with a button to clear the iframe.
 function openR() {
